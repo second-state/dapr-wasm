b1f9f322b2ea58c879c2160705b79d9254415573
yukang <moorekang@gmail.com>
2021-08-17 13:06:15 +0000

    explore rust grayscale

--- image-classification/src/lib.rs
+++ image-classification/src/lib.rs
@@ -1,7 +1,8 @@
-use wasm_bindgen::prelude::*;
+/* use wasm_bindgen::prelude::*;
 mod infer;
 
 #[wasm_bindgen]
 pub fn infer(image_data: &[u8]) -> String {
     infer::infer_internal(image_data)
 }
+ */
