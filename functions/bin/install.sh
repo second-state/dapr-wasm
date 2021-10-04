13941fa33f2f913be8b408b12167e2354a0b035f
yukang <moorekang@gmail.com>
2021-10-04 09:36:46 +0000

    update to new version and fix pre-install

--- functions/bin/install.sh
+++ functions/bin/install.sh
@@ -3,23 +3,23 @@ sudo apt-get install -y libjpeg-dev libpng-dev
 
 wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O - | /bin/bash
 
-wget https://github.com/second-state/WasmEdge-go/releases/download/v0.8.1/install_wasmedge.sh
+wget https://github.com/second-state/WasmEdge-go/releases/download/v0.8.2/install_wasmedge.sh
 sudo bash ./install_wasmedge.sh /usr/local
-wget https://github.com/second-state/WasmEdge-go/releases/download/v0.8.1/install_wasmedge_tensorflow_deps.sh
-wget https://github.com/second-state/WasmEdge-go/releases/download/v0.8.1/install_wasmedge_tensorflow.sh
+wget https://github.com/second-state/WasmEdge-go/releases/download/v0.8.2/install_wasmedge_tensorflow_deps.sh
+wget https://github.com/second-state/WasmEdge-go/releases/download/v0.8.2/install_wasmedge_tensorflow.sh
 sudo bash ./install_wasmedge_tensorflow_deps.sh /usr/local
 sudo bash ./install_wasmedge_tensorflow.sh /usr/local
 
-wget https://github.com/second-state/WasmEdge-go/releases/download/v0.8.1/install_wasmedge_image.sh
+wget https://github.com/second-state/WasmEdge-go/releases/download/v0.8.2/install_wasmedge_image.sh
 sudo bash ./install_wasmedge_image.sh /usr/local
 
 FILE="./wasmedgec-tensorflow"
 if [ ! -f "$FILE" ]; then
     echo "install $FILE ..."
-    curl -L https://github.com/second-state/WasmEdge-tensorflow-tools/releases/download/0.8.2-rc2/WasmEdge-tensorflow-tools-0.8.2-rc2-manylinux2014_x86_64.tar.gz -o ./WasmEdge-tensorflow-tools-0.8.2-rc2-manylinux2014_x86_64.tar.gz
-    tar xzvf WasmEdge-tensorflow-tools-0.8.2-rc2-manylinux2014_x86_64.tar.gz wasmedge-tensorflow-lite
-    tar xzvf WasmEdge-tensorflow-tools-0.8.2-rc2-manylinux2014_x86_64.tar.gz wasmedgec-tensorflow
-    rm WasmEdge-tensorflow-tools-0.8.2-rc2-manylinux2014_x86_64.tar.gz
+    curl -L https://github.com/second-state/WasmEdge-tensorflow-tools/releases/download/0.8.2/WasmEdge-tensorflow-tools-0.8.2-manylinux2014_x86_64.tar.gz -o ./WasmEdge-tensorflow-tools-0.8.2-manylinux2014_x86_64.tar.gz
+    tar xzvf WasmEdge-tensorflow-tools-0.8.2-manylinux2014_x86_64.tar.gz wasmedge-tensorflow-lite
+    tar xzvf WasmEdge-tensorflow-tools-0.8.2-manylinux2014_x86_64.tar.gz wasmedgec-tensorflow
+    rm WasmEdge-tensorflow-tools-0.8.2-manylinux2014_x86_64.tar.gz
 
     curl -L https://github.com/second-state/WasmEdge-tensorflow-deps/releases/download/0.8.0/WasmEdge-tensorflow-deps-TFLite-0.8.0-manylinux2014_x86_64.tar.gz -o ./WasmEdge-tensorflow-deps-TFLite-0.8.0-manylinux2014_x86_64.tar.gz
     tar xzvf WasmEdge-tensorflow-deps-TFLite-0.8.0-manylinux2014_x86_64.tar.gz
@@ -32,4 +32,5 @@ cp libtensorflowlite_c.so ../../image-api-rs/lib
 cp wasmedge-tensorflow-lite ../../image-api-go/lib
 cp libtensorflowlite_c.so ../../image-api-go/lib
 
+curl https://raw.githubusercontent.com/second-state/rustwasmc/master/installer/init.sh -sSf | sh
 
--- image-api-rs/run_api_rs.sh
+++ image-api-rs/run_api_rs.sh
@@ -4,4 +4,4 @@ dapr run --app-id image-api-rs \
          --dapr-http-port 3502 \
          --components-path ../config \
          --log-level debug \
-         ./target/release/image-api-rs
+         ./target/debug/image-api-rs
