1148f94c9f3d123902d369415934236e2507112a
yukang <moorekang@gmail.com>
2021-10-04 09:45:40 +0000

    add dapr init

--- functions/bin/install.sh
+++ functions/bin/install.sh
@@ -2,6 +2,8 @@ sudo apt-get update
 sudo apt-get install -y libjpeg-dev libpng-dev
 
 wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O - | /bin/bash
+## require docker run in non-root user, refer to https://docs.docker.com/engine/install/linux-postinstall/
+dapr init
 
 wget https://github.com/second-state/WasmEdge-go/releases/download/v0.8.2/install_wasmedge.sh
 sudo bash ./install_wasmedge.sh /usr/local
