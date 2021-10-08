41950e4dfc99636f5d1ed01ce2036742adfb826b
yukang <moorekang@gmail.com>
2021-10-08 03:04:09 +0000

    update

--- functions/bin/install.sh
+++ functions/bin/install.sh
@@ -5,34 +5,21 @@ wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O 
 ## require docker run in non-root user, refer to https://docs.docker.com/engine/install/linux-postinstall/
 dapr init
 
-wget https://github.com/second-state/WasmEdge-go/releases/download/v0.8.2/install_wasmedge.sh
-sudo bash ./install_wasmedge.sh /usr/local
-wget https://github.com/second-state/WasmEdge-go/releases/download/v0.8.2/install_wasmedge_tensorflow_deps.sh
-wget https://github.com/second-state/WasmEdge-go/releases/download/v0.8.2/install_wasmedge_tensorflow.sh
-sudo bash ./install_wasmedge_tensorflow_deps.sh /usr/local
-sudo bash ./install_wasmedge_tensorflow.sh /usr/local
+wget -qO- https://raw.githubusercontent.com/WasmEdge/WasmEdge/master/utils/install.sh | sudo bash -s -- -e all
 
-wget https://github.com/second-state/WasmEdge-go/releases/download/v0.8.2/install_wasmedge_image.sh
-sudo bash ./install_wasmedge_image.sh /usr/local
 
-FILE="./wasmedgec-tensorflow"
-if [ ! -f "$FILE" ]; then
-    echo "install $FILE ..."
-    curl -L https://github.com/second-state/WasmEdge-tensorflow-tools/releases/download/0.8.2/WasmEdge-tensorflow-tools-0.8.2-manylinux2014_x86_64.tar.gz -o ./WasmEdge-tensorflow-tools-0.8.2-manylinux2014_x86_64.tar.gz
-    tar xzvf WasmEdge-tensorflow-tools-0.8.2-manylinux2014_x86_64.tar.gz wasmedge-tensorflow-lite
-    tar xzvf WasmEdge-tensorflow-tools-0.8.2-manylinux2014_x86_64.tar.gz wasmedgec-tensorflow
-    rm WasmEdge-tensorflow-tools-0.8.2-manylinux2014_x86_64.tar.gz
+#wget https://github.com/second-state/WasmEdge-go/releases/download/v0.8.2/install_wasmedge.sh
+#sudo bash ./install_wasmedge.sh /usr/local
 
-    curl -L https://github.com/second-state/WasmEdge-tensorflow-deps/releases/download/0.8.0/WasmEdge-tensorflow-deps-TFLite-0.8.0-manylinux2014_x86_64.tar.gz -o ./WasmEdge-tensorflow-deps-TFLite-0.8.0-manylinux2014_x86_64.tar.gz
-    tar xzvf WasmEdge-tensorflow-deps-TFLite-0.8.0-manylinux2014_x86_64.tar.gz
-    rm WasmEdge-tensorflow-deps-TFLite-0.8.0-manylinux2014_x86_64.tar.gz
-fi
 
-cp wasmedge-tensorflow-lite ../../image-api-rs/lib
-cp libtensorflowlite_c.so ../../image-api-rs/lib
+#wget https://github.com/second-state/WasmEdge-go/releases/download/v0.8.2/install_wasmedge_tensorflow_deps.sh
+#wget https://github.com/second-state/WasmEdge-go/releases/download/v0.8.2/install_wasmedge_tensorflow.sh
+#sudo bash ./install_wasmedge_tensorflow_deps.sh /usr/local
+#sudo bash ./install_wasmedge_tensorflow.sh /usr/local
 
-cp wasmedge-tensorflow-lite ../../image-api-go/lib
-cp libtensorflowlite_c.so ../../image-api-go/lib
+#wget https://github.com/second-state/WasmEdge-go/releases/download/v0.8.2/install_wasmedge_image.sh
+#sudo bash ./install_wasmedge_image.sh /usr/local
 
-curl https://raw.githubusercontent.com/second-state/rustwasmc/master/installer/init.sh -sSf | sh
+
+#curl https://raw.githubusercontent.com/second-state/rustwasmc/master/installer/init.sh -sSf | sh
 
