23f02167a5c08eefb6623b0391893fbf6ce5fa5d
yukang <moorekang@gmail.com>
2021-10-08 03:59:54 +0000

    fix script

--- functions/bin/install.sh
+++ functions/bin/install.sh
@@ -5,21 +5,5 @@ wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O 
 ## require docker run in non-root user, refer to https://docs.docker.com/engine/install/linux-postinstall/
 dapr init
 
-wget -qO- https://raw.githubusercontent.com/WasmEdge/WasmEdge/master/utils/install.sh | sudo bash -s -- -e all
-
-
-#wget https://github.com/second-state/WasmEdge-go/releases/download/v0.8.2/install_wasmedge.sh
-#sudo bash ./install_wasmedge.sh /usr/local
-
-
-#wget https://github.com/second-state/WasmEdge-go/releases/download/v0.8.2/install_wasmedge_tensorflow_deps.sh
-#wget https://github.com/second-state/WasmEdge-go/releases/download/v0.8.2/install_wasmedge_tensorflow.sh
-#sudo bash ./install_wasmedge_tensorflow_deps.sh /usr/local
-#sudo bash ./install_wasmedge_tensorflow.sh /usr/local
-
-#wget https://github.com/second-state/WasmEdge-go/releases/download/v0.8.2/install_wasmedge_image.sh
-#sudo bash ./install_wasmedge_image.sh /usr/local
-
-
-#curl https://raw.githubusercontent.com/second-state/rustwasmc/master/installer/init.sh -sSf | sh
-
+wget -qO- https://raw.githubusercontent.com/WasmEdge/WasmEdge/master/utils/install.sh | sudo bash -s -- -e all -p /usr/local
+curl https://raw.githubusercontent.com/second-state/rustwasmc/master/installer/init.sh -sSf | sh
