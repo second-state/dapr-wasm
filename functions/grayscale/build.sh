25a91f96862b455ae3030970e632b60ac3b16dd2
yukang <moorekang@gmail.com>
2022-05-04 14:54:04 +0000

    fix build.sh

--- functions/grayscale/build.sh
+++ functions/grayscale/build.sh
@@ -2,6 +2,7 @@ rustup override set 1.58.0
 rustup target add wasm32-wasi
 cargo build --target wasm32-wasi --release
 
+wasmedgec ./target/wasm32-wasi/release/grayscale.wasm ./target/wasm32-wasi/release/grayscale.wasm
 cp ./target/wasm32-wasi/release/grayscale.wasm ../../image-api-rs/lib
 cp ./target/wasm32-wasi/release/grayscale_lib.wasm ../../image-api-go/lib
 echo -e "finished build functions/grayscale ..."
