efe0049c2c0281ea1977c2e880c76fe01a8aece7
Michael Yuan <michael@secondstate.io>
2022-10-09 03:35:33 +0800

    Update run_classify.sh
    Signed-off-by: Michael Yuan <michael@michaelyuan.com>

--- image-api-classify/run_classify.sh
+++ image-api-classify/run_classify.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 cargo build --target wasm32-wasi --release
+wasmedgec target/wasm32-wasi/release/wasmedge_hyper_server_tflite.wasm wasmedge_hyper_server_tflite.wasm
 dapr stop image-api-classify
 dapr run --app-id image-api-classify \
         --app-protocol http \
@@ -8,4 +9,4 @@ dapr run --app-id image-api-classify \
         --dapr-http-port 3504 \
         --log-level debug \
         --components-path ../config \
-        wasmedge-tensorflow-lite target/wasm32-wasi/release/wasmedge_hyper_server_tflite.wasm
+        wasmedge-tensorflow-lite wasmedge_hyper_server_tflite.wasm
