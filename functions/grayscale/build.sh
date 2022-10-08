81bdf488e17361d2d3195eed11ade04c4cc02603
yukang <moorekang@gmail.com>
2022-10-08 04:43:51 +0000

    clean up functions

--- functions/grayscale/build.sh
+++
@@ -1,7 +0,0 @@
-rustup override set 1.58.0
-rustup target add wasm32-wasi
-cargo clean
-cargo build --target wasm32-wasi --release
-wasmedgec ./target/wasm32-wasi/release/grayscale.wasm ./target/wasm32-wasi/release/grayscale.wasm
-wasmedgec ./target/wasm32-wasi/release/grayscale_lib.wasm ./target/wasm32-wasi/release/grayscale_lib.wasm
-echo -e "finished build functions/grayscale ..."
