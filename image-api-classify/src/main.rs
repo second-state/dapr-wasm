14af9f6ca6976da723c0c4f71290151c80a2b180
Michael Yuan <michael@secondstate.io>
2023-05-13 07:10:27 +0000

    Use wasi nn
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- image-api-classify/src/main.rs
+++ image-api-classify/src/main.rs
@@ -4,14 +4,29 @@ use hyper::header::*;
 use std::convert::Infallible;
 use std::net::SocketAddr;
 use std::result::Result;
+use std::io::Cursor;
+use std::fs;
+use image::io::Reader;
+use image::DynamicImage;
+use std::convert::TryInto;
 use chrono::prelude::*;
 use serde_json::json;
+use wasi_nn;
 
 /// This is our service handler. It receives a Request, routes on its
 /// path, and returns a Future of a Response.
 async fn classify(req: Request<Body>) -> Result<Response<Body>, anyhow::Error> {
-    let model_data: &[u8] = include_bytes!("models/mobilenet_v1_1.0_224/mobilenet_v1_1.0_224_quant.tflite");
+    let graph = unsafe {
+        wasi_nn::load(
+            &[&fs::read("models/mobilenet_v1_1.0_224/mobilenet_v1_1.0_224_quant.tflite").unwrap()],
+            4, // encoding for tflite: wasi_nn::GRAPH_ENCODING_TENSORFLOWLITE
+            wasi_nn::EXECUTION_TARGET_CPU,
+        )
+        .unwrap()
+    };
+    let context = unsafe { wasi_nn::init_execution_context(graph).unwrap() };
     let labels = include_str!("models/mobilenet_v1_1.0_224/labels_mobilenet_quant_v1_224.txt");
+
     match (req.method(), req.uri().path()) {
         // Serve some instructions at /
         (&Method::GET, "/") => Ok(Response::new(Body::from(
@@ -29,30 +44,34 @@ async fn classify(req: Request<Body>) -> Result<Response<Body>, anyhow::Error> {
             println!("IP is {}", ip);
 
             let buf = hyper::body::to_bytes(req.into_body()).await?;
-            let flat_img = wasmedge_tensorflow_interface::load_jpg_image_to_rgb8(&buf, 224, 224);
-
-            let mut session = wasmedge_tensorflow_interface::Session::new(&model_data, wasmedge_tensorflow_interface::ModelType::TensorFlowLite);
-            session.add_input("input", &flat_img, &[1, 224, 224, 3])
-                .run();
-            let res_vec: Vec<u8> = session.get_output("MobilenetV1/Predictions/Reshape_1");
-
-            let mut i = 0;
-            let mut max_index: i32 = -1;
-            let mut max_value: u8 = 0;
-            while i < res_vec.len() {
-                let cur = res_vec[i];
-                if cur > max_value {
-                    max_value = cur;
-                    max_index = i as i32;
-                }
-                i += 1;
+            let tensor_data = image_to_tensor(&buf, 224, 224);
+            let tensor = wasi_nn::Tensor {
+                dimensions: &[1, 224, 224, 3],
+                r#type: wasi_nn::TENSOR_TYPE_U8,
+                data: &tensor_data,
+            };
+            unsafe {
+                wasi_nn::set_input(context, 0, tensor).unwrap();
             }
-
-            let mut label_lines = labels.lines();
-            for _i in 0..max_index {
-                label_lines.next();
+            // Execute the inference.
+            unsafe {
+                wasi_nn::compute(context).unwrap();
             }
-            let class_name = label_lines.next().unwrap().to_string();
+            // Retrieve the output.
+            let mut output_buffer = vec![0u8; labels.lines().count()];
+            unsafe {
+                wasi_nn::get_output(
+                    context,
+                    0,
+                    &mut output_buffer[..] as *mut [u8] as *mut u8,
+                    output_buffer.len().try_into().unwrap(),
+                )
+                .unwrap();
+            }
+            // Sort the result with the highest probability result first
+            let results = sort_results(&output_buffer);
+            // The first result's first element points to the labels position
+            let class_name = labels.lines().nth(results[0].0).unwrap_or("Unknown");
             println!("result: {}", class_name);
 
             // Connect to local sidecar
@@ -72,7 +91,7 @@ async fn classify(req: Request<Body>) -> Result<Response<Body>, anyhow::Error> {
             println!("KVS is {}", serde_json::to_string(&kvs)?);
             client.save_state("statestore", kvs).await?;
 
-            Ok(Response::new(Body::from(format!("{} is detected with {}/255 confidence", class_name, max_value))))
+            Ok(Response::new(Body::from(format!("{} is detected with {}/255 confidence", class_name, results[0].1))))
         }
 
         // Return the 404 Not Found for other routes.
@@ -116,3 +135,34 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     }
     */
 }
+
+// Sort the buffer of probabilities. The graph places the match probability for each class at the
+// index for that class (e.g. the probability of class 42 is placed at buffer[42]). Here we convert
+// to a wrapping InferenceResult and sort the results.
+fn sort_results(buffer: &[u8]) -> Vec<InferenceResult> {
+    let mut results: Vec<InferenceResult> = buffer
+        .iter()
+        .enumerate()
+        .map(|(c, p)| InferenceResult(c, *p))
+        .collect();
+    results.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
+    results
+}
+
+// Take the image data, resize it to height x width, and then convert
+// the pixel precision to FP32. The resulting BGR pixel vector is then returned.
+fn image_to_tensor(raw_data: &[u8], height: u32, width: u32) -> Vec<u8> {
+    let reader = Reader::new(Cursor::new(raw_data))
+        .with_guessed_format()
+        .expect("Cursor io never fails");
+    let pixels = reader.decode().unwrap();
+    let dyn_img: DynamicImage = pixels.resize_exact(width, height, image::imageops::Triangle);
+    let bgr_img = dyn_img.to_rgb8();
+    // Get an array of the pixel values
+    let raw_u8_arr: &[u8] = &bgr_img.as_raw()[..];
+    return raw_u8_arr.to_vec();
+}
+
+// A wrapper for class ID and match probabilities.
+#[derive(Debug, PartialEq)]
+struct InferenceResult(usize, u8);
